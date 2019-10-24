88613f2dc533ee52e56252f315f6ca85f343fcc5
c-consalpa <konstantin.alpashkin@gmail.com>
2019-10-25 01:55:48 +0300

    projects synced

--- src/IO/XOM/XOMBase.java
+++ src/IO/XOM/XOMBase.java
@@ -1,59 +1,59 @@
 package IO.XOM;
 
-import nu.xom.*;
+
 
 import javax.print.Doc;
 import java.io.*;
 
 public class XOMBase {
-    public static void main(String[] args) {
-    Person person = new Person("1123123", "asdadwd");
-
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream("xomData");
-            Serializer s = new Serializer(fileOutputStream, "UTF-8");
-            s.setIndent(4);
-            s.setMaxLength(64);
-            Document document = new Document(person.getAsXML());
-            s.write(document);
-            s.flush();
-            fileOutputStream.close();
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-
-//      read:
-
-        try {
-            Builder parser = new Builder();
-            Document doc = parser.build(new File("xomData"));
-            System.out.println("restored:");
-            System.out.println(doc.toXML());
-        } catch (IOException e) {
-            System.err.println(e);
-        } catch (ValidityException e) {
-            e.printStackTrace();
-        } catch (ParsingException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-class Person {
-    public String name;
-    public String lastName;
-
-    public Person(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
-
-    public Element getAsXML() {
-        Element element = new Element("Person");
-        Element fName = new Element("first");
-        fName.appendChild(name);
-        element.appendChild(fName);
-
-        return element;
-    }
+//    public static void main(String[] args) {
+//    Person person = new Person("1123123", "asdadwd");
+//
+//        try {
+//            FileOutputStream fileOutputStream = new FileOutputStream("xomData");
+//            Serializer s = new Serializer(fileOutputStream, "UTF-8");
+//            s.setIndent(4);
+//            s.setMaxLength(64);
+//            Document document = new Document(person.getAsXML());
+//            s.write(document);
+//            s.flush();
+//            fileOutputStream.close();
+//        } catch (IOException e) {
+//            System.err.println(e);
+//        }
+//
+////      read:
+//
+//        try {
+//            Builder parser = new Builder();
+//            Document doc = parser.build(new File("xomData"));
+//            System.out.println("restored:");
+//            System.out.println(doc.toXML());
+//        } catch (IOException e) {
+//            System.err.println(e);
+//        } catch (ValidityException e) {
+//            e.printStackTrace();
+//        } catch (ParsingException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+//
+//class Person {
+//    public String name;
+//    public String lastName;
+//
+//    public Person(String name, String lastName) {
+//        this.name = name;
+//        this.lastName = lastName;
+//    }
+//
+//    public Element getAsXML() {
+//        Element element = new Element("Person");
+//        Element fName = new Element("first");
+//        fName.appendChild(name);
+//        element.appendChild(fName);
+//
+//        return element;
+//    }
 }
